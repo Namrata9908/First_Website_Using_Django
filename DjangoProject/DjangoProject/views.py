ccf1b9e2fdbe098d7608f7f5a15e695199593b6e
namratattanajidalvi <namratatd0062@gmail.com>
2025-07-27 20:59:05 +0530

    update

---
+++ DjangoProject/DjangoProject/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'index.html')
+
+def about(request):
+    return HttpResponse('This is about Page')
+
+def more_info(request):
+    return HttpResponse('More Information ')
